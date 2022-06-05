2820b71c8d4b70ecbadf105e2637a1e160c4d722
J. Philip Pampreen <99842527+LanzFriszt@users.noreply.github.com>
2022-06-05 19:48:19 -0400

    Update Mishler_Homework_1.py

--- Mishler_Homework_1.py
+++ Mishler_Homework_1.py
@@ -3,7 +3,7 @@ from math import sqrt
 print (randint(1,10))
 for i in range(int(sqrt(10000))+1):
     print (i**2)
-abab = "aaabbbbbbbbbaaaabababbbbbaaaaaaaa"
+abab = open("abab.txt", "r").readlines()[0]
 print (len([_ for _ in filter(lambda x: x == 'b', abab)]))
 print (max(len(x) for x in abab.split('b')) - 1)
 print (max(len(x) for x in abab.split('a')) - 1)
