eeb60e399dcf22a705e8c23731aef7ac54daeadd
J. Philip Pampreen <99842527+LanzFriszt@users.noreply.github.com>
2022-06-05 19:52:08 -0400

    Update Mishler_Homework_1.py

--- Mishler_Homework_1.py
+++ Mishler_Homework_1.py
@@ -1,9 +1,13 @@
-from random import randint
 from math import sqrt
+from random import randint
+#Problem 1
 print (randint(1,10))
+#Problem 2
 for i in range(int(sqrt(10000))+1):
     print (i**2)
+#Problem 3
 abab = open("abab.txt", "r").readlines()[0]
 print (len([_ for _ in filter(lambda x: x == 'b', abab)]))
+#Problem 4
 print (max(len(x) for x in abab.split('b')) - 1)
 print (max(len(x) for x in abab.split('a')) - 1)
